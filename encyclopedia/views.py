ef26dd8fa15cf2e605f9933ee6a90f285fce01a6
achrefabdennebi <achref.abdennebi@gmail.com>
2020-07-14 12:22:16 +0200

    feat: add title in head
    feat: add entrypage

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,7 +9,16 @@ def index(request):
     })
 
 def entryPage(request, title):
+    entry = util.get_entry(title)
+
+    if entry == None:
+        return render(request, "encyclopedia/error.html", {
+            "title": "{}: Entry not found".format(title.capitalize()),
+            "error" : "Oops ! entry not exited, please enter an existing entry."
+        })
+
     return render(request, "encyclopedia/entrypage.html", {
-        "title" : title
+        "title" : title.capitalize(),
+        "entry": entry
     })
 
