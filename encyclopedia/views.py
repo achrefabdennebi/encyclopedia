ecf4bb5c998f0fcc884ae33d53ce3d9f1c13f147
achrefabdennebi <achref.abdennebi@gmail.com>
2020-07-14 13:55:15 +0200

    feat: add search behavior

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from . import util
 
-
+from re import search as searchSubstr
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -22,3 +22,23 @@ def entryPage(request, title):
         "entry": entry
     })
 
+def search(request):
+    if request.method == "GET":
+        keyword = request.GET['q']
+        entry = util.get_entry(keyword)
+        if entry != None:
+            return render(request, "encyclopedia/entrypage.html", {
+                "title" : keyword.capitalize(),
+                "entry": entry
+            })
+
+    entries = util.list_entries()
+    search_results = []
+    for name in entries:
+        if searchSubstr(keyword.upper(), name.upper()):
+            search_results.append(name)
+
+    return render(request, "encyclopedia/searchResults.html", {
+    "title": "Search Results: {}".format(keyword),
+    "entries": search_results
+    })
